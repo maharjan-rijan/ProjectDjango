f8f047a4f197cb96a47af1c93ee0c84bc76072cf
Rijan Maharjan <rijanq0@gmail.com>
2024-07-02 21:15:54 +0545

    django project

---
+++ Municipality/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homePage(request):
+    return render(request, "index.html")
+
+def aboutUs(request):
+    return render(request,"about.html")
+
+def contact(request):
+    return render(request,"contact.html")
