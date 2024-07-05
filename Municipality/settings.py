47f97951d31df0d10088175426b2f2206b94a84f
Rijan Maharjan <rijanq0@gmail.com>
2024-07-05 07:57:21 +0545

    some modle file

--- Municipality/settings.py
+++ Municipality/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nepali_datetime_field',
+    'main',
+    'departments',
+    # 'nepali_datetime_field',
 ]
 
 MIDDLEWARE = [
