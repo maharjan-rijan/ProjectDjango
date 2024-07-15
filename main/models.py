8cb74c563cbeff124f71422561cab2dc7194bf4f
Rijan Maharjan <rijanq0@gmail.com>
2024-07-15 16:03:54 +0545

    Style Change

--- main/models.py
+++ main/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
-STAFFS = (
+CATEGORY = (
     ("electionOfficial", "electionOfficial"),
     ("multipalStaffs", "multipalStaffs"),
     ("wardMembers", "wardMembers"),
+)
+POSITION = ( 
+    ("mayor", "mayor"),
+    ("deputy-mayor", "deputy-mayor"),
+    ("Deputy Secretary", "Deputy Secretary"),
+    ("Branch Officer","Branch Officer"),
+    ("Accounts Officer","Accounts Officer"),
+    ("engineer", "engineer"),
 )
 # Create your models here.
 class About(models.Model):
@@ -14,10 +22,11 @@ class About(models.Model):
     description=models.TextField()
     
 class Member(models.Model):
+    category=models.CharField(max_length=50,choices=CATEGORY,default=None,null=True)
     name=models.CharField(max_length=100,default=None)
     slug=models.SlugField(unique=True,null=True,default=None)
     image=models.FileField(upload_to="staffs/",max_length=255,null=True,default=None)
     designation=models.CharField(max_length=100,default=None)
     contact=models.CharField(max_length=100,null=True)
     email=models.EmailField(max_length=300,null=True)
-    position=models.CharField(max_length=50,choices=STAFFS,default=None)
+    position=models.CharField(max_length=50,choices=POSITION,default=None)
